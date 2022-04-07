9b6de6f749feba9913f066b65c6dd93c15ea6429
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-07 19:26:01 +0530

    1 file modified

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,7 +3,10 @@ import classes from './Header.module.css';
 const Header = (props) => {
   return;
   <Fragment>
-    <header className={classes.Header}>Header will come</header>
+    <header className={classes.Header}>
+      <h1>React Meals</h1>
+      <button type="button">Cart</button>
+    </header>
     <div>Image will come here</div>
   </Fragment>;
 };
